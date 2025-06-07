764b5174468e1386b2547afb62459fe475e3be13
Aline Miranda <alinemiranda879@gmail.com>
2025-06-06 21:35:39 -0300

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props) {
   const { title, subtitle } = props
   return(
-    <div>
+    <div className="Pokédex">
       <h1>{title}</h1>
       <h3>{subtitle}</h3>
     </div> 
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,32 @@
+// src/components/PokemonCard.jsx
+import React, { useState } from 'react';
+
+function PokemonCard({ id, name, type, image }) {
+  // Define um estado local 'showInfo' para controlar exibição de detalhes
+  const [showInfo, setShowInfo] = useState(false);
+
+  // Função para alternar o estado showInfo
+  const toggleInfo = () => {
+    setShowInfo(prevShowInfo => !prevShowInfo);
+  };
+
+  return (
+    <div className="pokemon-card">
+      <h3>#{id.toString().padStart(3, '0')} {name}</h3>
+      <h3 className="name">{name}</h3>
+      <img src={image} alt={name} width="100" height="100" />
+      
+      {/* Botão para mostrar/ocultar detalhes */}
+      <button onClick={toggleInfo}>
+        { showInfo ? 'Esconder Detalhes' : 'Mostrar Detalhes' }
+      </button>
+
+      {/* Renderização condicional do tipo: aparece somente se showInfo for true */}
+      {showInfo && (
+        <p className='tipo'>Tipo: {type}</p>
+      )}
+    </div>
+  );
+}
+
+export default PokemonCard;
